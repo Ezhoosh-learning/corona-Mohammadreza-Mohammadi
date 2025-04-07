d0dd007e79ddc8e6f5a1b968eceeffb3d26315a0
Mohammadreza <mohammadreza.mohammadi8313@gmail.com>
2025-04-07 20:04:56 +0330

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@ const data = {
   datasets: [
     {
       label: "My First Dataset",
-      data: [65, 59, 80, 81, 56, 55, 40],
+      data: [0, 20, 120, 10, 15, 90, 75],
       fill: false,
       borderColor: "rgb(75, 192, 192)",
       tension: 0.1,
